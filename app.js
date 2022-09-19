1e381c311886442917f45b92c21d11fc50e7ec23
dariusflonta <110110173+dariusflonta@users.noreply.github.com>
2022-09-20 01:09:23 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ window.addEventListener('scroll', revealOne);
         for(var i = 0; i < reveals.length; i++){
             var windowHeight = window.innerHeight;
             var revealtop = reveals[i].getBoundingClientRect().top;
-            var revealpoint = 40;
+            var revealpoint = 20;
 
             if(revealtop < windowHeight - revealpoint) {
                 reveals[i].classList.add('activeReveal');
                 }
             }
         };
+
+/*document.querySelectorAll('.open-img').forEach(image =>{
+    image.onclick = () => {
+        document.querySelector('.pop-up').style.display = 'block';
+        document.querySelector('pop-up img').src = image.getAttribute('src');
+    }
+});
+
+document.querySelector('.close-img').onclick=() => {document.querySelector('.pop-up').style.display = 'none';
+}*/
